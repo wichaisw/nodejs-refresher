0ae0016db06cb5a9cb55b25d6ec00732fdc3a7fe
wichaisw <wichai.sw19@gmail.com>
2021-08-04 02:01:30 +0700

    ADD vanilla node.js routing and request, response handling

--- 03-basic-nodejs/app.js
+++ 03-basic-nodejs/app.js
@@ -1,10 +1,45 @@
 const http = require('http');
+const fs = require('fs');
 
 // execute anonymous callback reqeustListener function when there is request
 const server = http.createServer((req, res) => {
-  console.log(req)
+  const url = req.url;
+  const method = req.method;
+
+  if(url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message-input" /><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if(url === '/message' && method === 'POST') {
+    const body = [];
+
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk);
+    });
+    req.on('end',() => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write('<html>');
+  res.write('<head><title>First Page</title></head>');
+  res.write('<body><h1>Test Node.js server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(8000, () => {
-  console.log("server is running on port 8000")
+  console.log("server is running on port 8000");
 });
