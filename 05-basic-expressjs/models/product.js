042edafa255fd5481802c58a1042480e9b2f165c
wichaisw <wichai.sw19@gmail.com>
2021-10-24 22:42:22 +0700

    refactor: refactor Product's readfile

--- 05-basic-expressjs/models/product.js
+++ 05-basic-expressjs/models/product.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 const rootDir = require('../utils/path');
 
-const productsFile = path.join(
+const productsFilePath = path.join(
   rootDir,
   'data',  
   'products.json'
 );
 
 const getProductsFromFile = callBack => {
-  
+  // should read stream instead if the file is quite large
+  fs.readFile(productsFilePath, (err, fileContent) => {
+    if(err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  })
 }
 class Product {
   constructor(title) {
@@ -19,16 +26,9 @@ class Product {
 
   save() {
     // this refer to the obj created based on this class
-    // products.push(this);
-
-    // should read steeam instead if the file is quite large
-    fs.readFile(productsFile, (err, fileContent) => {
-      let products = [];
-      if(!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(productsFile, JSON.stringify(products), err => {
+      fs.writeFile(productsFilePath, JSON.stringify(products), err => {
         console.debug(err);
       } );
     });
@@ -36,15 +36,8 @@ class Product {
 
   // make you able to call fetchAll from the class itself without new instantiation
   static fetchAll(callBack) {
-    fs.readFile(productsFile, (err, fileContent) => {
-      if(err) callBack([]);
-
-      callBack(JSON.parse(fileContent));
-    })
+    getProductsFromFile(callBack);
   }
 }  
 
 module.exports = Product;
-
-// exports.Product = Product;
-// exports.products = products;
